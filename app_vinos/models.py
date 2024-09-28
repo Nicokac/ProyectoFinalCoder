be698c6c1e5a498778d1cf21321798a8107fcfb8
Nicokac <kachuk_nm@hotmail.com>
2024-09-28 10:46:25 -0300

    Se crean modelos en la base de datos

--- app_vinos/models.py
+++ app_vinos/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+class Preferencia(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE) #El parámetro on_delete=models.CASCADE especifica que si se elimina un Usuario, todas sus Preferencias asociadas también serán eliminadas.
+    sabor = models.CharField(max_length=100)
+    intensidad = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.sabor} ({self.intensidad})'
+    
+class Vino(models.Model):
+    nombre = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    sabor = models.CharField(max_length=100)
+    intensidad = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+    
+class Recomendacion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    vino = models.ForeignKey(Vino, on_delete=models.CASCADE)
+    preferencia = models.ForeignKey(Preferencia, on_delete=models.CASCADE)
+    comentario = models.TextField()
+
+    def __str__(self):
+        return f'Recomendación para {self.usuario.nombre} - {self.vino.nombre}'
+    
+    
+    
+      
