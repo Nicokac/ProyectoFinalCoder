46ff364e7d2fd8ce7ab6b17b4fe728bf19031283
Nicokac <kachuk_nm@hotmail.com>
2024-09-29 11:58:24 -0300

    Se agregaron los modelos al admin de django

--- app_vinos/admin.py
+++ app_vinos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario, Vino, Preferencia, Recomendacion
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Vino)
+admin.site.register(Preferencia)
+admin.site.register(Recomendacion)
+
--- app_vinos/models.py
+++ app_vinos/models.py
@@ -7,7 +7,13 @@ class Usuario(models.Model):
     email = models.EmailField()
 
     def __str__(self):
-        return self.nombre
+        return f'{self.nombre} {self.apellido} {self.email}'
+    
+    class Meta():
+
+        verbose_name = 'Usuarios'
+        verbose_name_plural = 'Mis usuarios'
+        ordering = ('nombre',)
 
 class Preferencia(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE) #El parámetro on_delete=models.CASCADE especifica que si se elimina un Usuario, todas sus Preferencias asociadas también serán eliminadas.
@@ -15,7 +21,7 @@ class Preferencia(models.Model):
     intensidad = models.IntegerField()
 
     def __str__(self):
-        return f'{self.sabor} ({self.intensidad})'
+        return f'{self.usuario} {self.sabor} ({self.intensidad})'
     
 class Vino(models.Model):
     nombre = models.CharField(max_length=100)
@@ -28,7 +34,13 @@ class Vino(models.Model):
     rs = models.DecimalField(max_digits=4, decimal_places=2, default=0.0, verbose_name='Residual Sugar (g/L)')
 
     def __str__(self):
-        return self.nombre
+        return f'{self.nombre} {self.tipo} {self.sabor} {self.intensidad} {self.abv} {self.ta} {self.rs}' 
+    
+    class Meta():
+
+        verbose_name = 'Vinos'
+        verbose_name_plural = 'Mis vinos'
+        ordering = ('nombre',)
     
 class Recomendacion(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
