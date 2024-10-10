0a7acfc0040ac978f2afced1f4314e588322ee9d
Nicokac <kachuk_nm@hotmail.com>
2024-10-09 21:06:20 -0300

    Se personaliza el apartado de admin

--- app_vinos/admin.py
+++ app_vinos/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import Usuario, Vino, Preferencia, Recomendacion, Avatar
 
+class UsuarioAdmin(admin.ModelAdmin):
+
+    list_display = ['nombre', 'apellido', 'email']
+    search_fields = ['nombre', 'apellido']
+    list_filter = ['nombre']
+
+class VinoAdmin(admin.ModelAdmin):
+
+    list_display = ['nombre', 'tipo', 'sabor', 'intensidad', 'abv', 'ph', 'ta', 'rs']
+    search_fields = ['nombre', 'tipo', 'sabor', 'intensidad', 'abv', 'ph', 'ta', 'rs']
+    list_filter = ['nombre', 'tipo', 'sabor', 'intensidad', 'abv', 'ph', 'ta', 'rs']
+
 # Register your models here.
-admin.site.register(Usuario)
-admin.site.register(Vino)
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Vino, VinoAdmin)
 admin.site.register(Preferencia)
 admin.site.register(Recomendacion)
 admin.site.register(Avatar)
